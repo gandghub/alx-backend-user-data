61a019df6f52dcca6b064d214145e05ce3758531
gandghub <hackthehacker33@gmail.com>
2024-09-19 14:38:49 +0000

    0x02-Session_authentication

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,2 +1,13 @@
-from api.v1.views.session_auth import *
+#!/usr/bin/env python3
+"""
+API view initialization file
+"""
 
+from flask import Blueprint
+
+# Initialize Blueprint for app views
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import views after initializing Blueprint to avoid circular imports
+from api.v1.views.users import *  # Adjust imports to avoid circular imports
+from api.v1.views.session_auth import *  # Adjust this if necessary
